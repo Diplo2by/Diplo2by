b0b684fd14119dd0f0bd83bad2ea1a9df4c0bb5e
Diplo2by <81457012+Diplo2by@users.noreply.github.com>
2021-06-13 13:23:39 +0530

    Delete Assignment_11.2.py

--- Assignment_11.2.py
+++
@@ -1,9 +0,0 @@
-import re
-f = open('regex.txt')
-li = list()
-for lines in f:
-    num = re.findall('[0-9]+',lines)
-    if len(num) > 0:
-        for i in num:
-            li.append(int(i))
-print(sum(li))
